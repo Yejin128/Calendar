59de70d998bb4544453b2a21636669ffb5f2c64b
Yejin128 <bbaekhuen@gmail.com>
2021-05-17 10:33:23 +0900

    Add repeat function

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -25,11 +25,17 @@ public class Calendar {
 		
 		Scanner sc=new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요");
+		
+		System.out.println("반복 횟수를 입력하세요.");
+		int repeat=sc.nextInt();
+		
+		System.out.println("월을 입력하세요");
+
+		for(int i=0;i<repeat;i++) {
 		int month=sc.nextInt();
 		
 		System.out.printf("%d월은 %d일까지 있습니다. \n",month,cal.getMaxDaysOfMonth(month));
-		
+		}
 		cal.printSampleCalendar();
 		sc.close();
 	}
